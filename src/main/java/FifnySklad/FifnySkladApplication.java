825a59c99d6260e7aaf9d8340c351cb1c303acfb
Arkadia <lajluch@gmail.com>
2022-05-15 10:16:54 +0200

    Initial commit

---
+++ src/main/java/FifnySklad/FifnySkladApplication.java
@@ -0,0 +1,13 @@
+package FifnySklad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifnySkladApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifnySkladApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FifnySklad/FifnySkladApplicationTests.java
@@ -0,0 +1,13 @@
+package FifnySklad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifnySkladApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
