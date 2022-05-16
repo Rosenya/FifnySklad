e1654905726661c33ad929e972141b6a073a75b8
Guthak <tomekomen1@gmail.com>
2022-05-16 18:34:45 +0200

    Tomek ver. 1.1

---
+++ src/main/java/FifnySklad/Controllers/HelloController.java
@@ -0,0 +1,14 @@
+package FifnySklad.Controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String getIndex() {
+
+        return "index";
+    }
+
+}
