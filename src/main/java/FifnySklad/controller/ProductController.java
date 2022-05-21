4befbdc4b354248b1c2adf8eba3bfaa981b7f4ec
Joanna Pawlas <joanna_pawlas@op.pl>
2022-05-21 13:16:33 +0200

    edycja kontrolerów

--- src/main/java/FifnySklad/controller/ProductController.java
+++ src/main/java/FifnySklad/controller/ProductController.java
@@ -6,17 +6,21 @@ import FifnySklad.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
 @Controller
 public class ProductController {
     private final ProductService productService;
-    private final ClientService clientService;
+//    private final ClientService clientService;
 
-    public ProductController(ProductService productService, ClientService clientService) {
+
+    public ProductController(ProductService productService) {
         this.productService = productService;
-        this.clientService = clientService;
     }
 
     @GetMapping("/products")
@@ -25,4 +29,29 @@ public class ProductController {
         model.addAttribute("products", products);
         return "products/products";
     }
+
+    @GetMapping("/addProduct")
+    public String addClient(){
+        return "products/addNewProduct";
+
+    }
+    @GetMapping("/editProduct/{id}")
+    public String getEditProduct(@PathVariable("id") Long id, Model model){
+        Product product = productService.getProduct(id);
+        model.addAttribute("product", product);
+        return "product/editProduct";
+    }
+    @PostMapping("/addProduct/{id}")
+    public RedirectView postEditProduct (@ModelAttribute Product newProduct, @PathVariable("id") Long id){
+        productService.editProduct(newProduct);
+        return new RedirectView("/editProduct/{id}");
+    }
+    @PostMapping("/editProduct/{id}")
+    public RedirectView deleteProduct(@PathVariable("id") Long id){
+        productService.deleteByIdProduct(id);
+        return new RedirectView("products");
+    }
+
+
+
 }
